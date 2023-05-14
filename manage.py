78857c53f0027f34e10248812ee005613a5f3705
Ishaq Azami <ishaqaza@buffalo.edu>
2023-05-14 15:56:58 -0400

    modify manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
+# Connect to the MySQL database
+db = mysql.connector.connect(
+    host='localhost',
+    user='flask',
+    password='ubuntu',
+    database='Project'
+)
+
+# Home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Reservation page
 @app.route('/reservation')
 def reservation():
     return render_template('reservation.html')
 
-@app.route('/confirmation')
+# Confirmation page
+@app.route('/confirmation', methods=['POST'])
 def confirmation():
-    return render_template('confirmation.html')
+    name = request.form['name']
+    date = request.form['date']
+    time = request.form['time']
+    party_size = request.form['party_size']
+    return render_template('confirmation.html', name=name, date=date, time=time, party_size=party_size)
 
+# Menu page
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM menu')
+    menu_items = cursor.fetchall()
+    cursor.close()
+    return render_template('menu.html', menu_items=menu_items)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
